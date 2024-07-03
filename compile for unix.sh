406b44c31b02e57c71889553883c6635b5a87a61
FelixBand <felixband@gmail.com>
2024-07-03 11:48:07 +0200

    rename to not include spaces

--- compile for unix.sh
+++
@@ -1,3 +0,0 @@
-rm -rf build
-rm -rf dist
-pyinstaller --onefile --windowed --icon=icon.icns -n Bandit "bandit pyqt.py"
--- compile for windows.bat
+++
@@ -1,4 +0,0 @@
-rd /s /q build
-rd /s /q dist
-pyinstaller --windowed --icon=icon.ico -n Bandit "bandit pyqt.py"
-pause
