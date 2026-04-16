bcb87dfa00b4fd3a43380f61c3e8dda517e36aa2
FelixBand <felixband@gmail.com>
2026-04-16 15:03:19 +0200

    fix proton pfx home path, forgot the "pfx" part

--- bandit.py
+++ bandit.py
@@ -121,9 +121,9 @@ elif platform.system() == "Linux":
             print("Failed to create system-wide folder. Please run this program as root or with sudo.")
             exit(1)
 
-    pfxhome = f"{os.path.expanduser('~')}/.banditpfx/drive_c/users/steamuser"
+    pfxhome = f"{os.path.expanduser('~')}/.banditpfx/pfx/drive_c/users/steamuser"
     if not os.path.exists(f"{os.path.expanduser('~')}/.banditpfx"):
-        subprocess.run(["sh", "-c", f"mkdir -p {os.path.expanduser('~')}/.banditpfx/drive_c/users/steamuser/AppData && ln -s {pfxhome}/AppData {os.path.expanduser('~')} &&ln -s {os.path.expanduser('~')}/Downloads {pfxhome} && ln -s {os.path.expanduser('~')}/Documents {pfxhome} && ln -s {os.path.expanduser('~')}/Desktop {pfxhome} && ln -s {os.path.expanduser('~')}/Music {pfxhome} && ln -s {os.path.expanduser('~')}/Pictures {pfxhome} && ln -s {os.path.expanduser('~')}/Videos {pfxhome}"])
+        subprocess.run(["sh", "-c", f"mkdir -p {os.path.expanduser('~')}/.banditpfx/pfx/drive_c/users/steamuser/AppData && ln -s {pfxhome}/AppData {os.path.expanduser('~')} &&ln -s {os.path.expanduser('~')}/Downloads {pfxhome} && ln -s {os.path.expanduser('~')}/Documents {pfxhome} && ln -s {os.path.expanduser('~')}/Desktop {pfxhome} && ln -s {os.path.expanduser('~')}/Music {pfxhome} && ln -s {os.path.expanduser('~')}/Pictures {pfxhome} && ln -s {os.path.expanduser('~')}/Videos {pfxhome}"])
 elif platform.system() == "Darwin":
     bandit_program_data = "/Library/Application Support/BanditGameLauncher"
 
